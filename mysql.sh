f8264b71511727d90e3004c822ec4907bb28c9ba
kalyan <kalyankalaven@gmail.com>
2025-12-27 12:23:20 +0530

    s expense shell

--- mysql.sh
+++ mysql.sh
@@ -30,7 +30,14 @@ mkdir -p "$LOGS_FOLDER"
 
 echo "Script started executing at: $TIMESTAMP" &>>"$LOG_FILE_NAME"
 
-CHECK_ROOT 
+CHECK_ROOT {
+if [ "$USERID" -ne 0 ]
+then
+  echo "ERROR:: You must have sudo access to execute this script"
+  exit 1
+fi
+}  
+CHECK_ROOT
 
 dnf install mysql-server -y &>>"$LOG_FILE_NAME"
 VALIDATE $? "Installing mysql-server"
