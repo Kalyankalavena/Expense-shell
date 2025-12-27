94ec92a30b9d22e58a442514995aba2f0e99db34
kalyan <kalyankalaven@gmail.com>
2025-12-27 11:06:26 +0530

    s added

---
+++ mysql.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#!/bin/bash
+echo -e "\nThis script demonstrates the use of colours in bash scripting\n"
+
+USERID=$(id -u)
+
+R="\e[0;31m"
+G="\e[0;32m"
+Y="\e[0;33m"
+NC="\e[0m"
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo "$0" | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/${LOG_FILE}-${TIMESTAMP}.log"
+
+VALIDATE() {
+  if [ $1 -ne 0 ]
+  then
+    echo -e "$2 ... ${R}FAILURE${NC}"
+    exit 1
+  else
+    echo -e "$2 ... ${G}SUCCESS${NC}"
+  fi
+}
+
+# Create logs folder if not exists
+mkdir -p "$LOGS_FOLDER"
+
+echo "Script started executing at: $TIMESTAMP" &>>"$LOG_FILE_NAME"
+
+CHECK_ROOT 
+
+dnf install mysql-server -y &>>"$LOG_FILE_NAME"
+VALIDATE $? "Installing mysql-server"
+
+systemctl enable mysqld &>>"$LOG_FILE_NAME"
+VALIDATE $? "Enabling mysql server"
+
+systemctl start mysqld &>>"$LOG_FILE_NAME"
+VALIDATE $? "Starting mysql server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1&>>"$LOG_FILE_NAME"
+VALIDATE $? "Setting mysql root password"
